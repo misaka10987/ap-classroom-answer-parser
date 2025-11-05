7c390ea78299bd682f9ddc2c539586ee281046e8
misaka10987 <misaka10987@outlook.com>
2025-11-05 08:07:35 +0800

    update build script

--- build.ts
+++ build.ts
@@ -3,6 +3,8 @@ import UnpluginTypia from '@ryoppippi/unplugin-typia/bun'
 import { mkdir } from 'fs/promises'
 import { readFile } from 'fs/promises'
 import { writeFile } from 'fs/promises'
+import { userscriptConfig } from './userscript.config'
+import { render } from '@jeiea/userscript-metadata'
 
 await spawn({ cmd: ['bun', 'run', 'check'] }).exited
 await build({
@@ -13,6 +15,6 @@ await build({
   plugins: [UnpluginTypia({ log: false })],
 })
 ;['out', 'dist'].forEach(async (dir) => await mkdir(dir, { recursive: true }))
-const meta = await readFile('meta.js')
+const meta = render(userscriptConfig)
 const body = await readFile('out/index.js')
 await writeFile('dist/ap-classroom-answer-parser.user.js', meta + '\n' + body)
---
+++ userscript.config.ts
@@ -0,0 +1,12 @@
+import pack from './package.json' assert { type: 'json' }
+import { type Metadata } from '@jeiea/userscript-metadata'
+
+export const userscriptConfig: Metadata = {
+  name: [pack.name],
+  namespace: [pack.homepage],
+  version: [pack.version],
+  author: [pack.author.name],
+  description: [pack.description],
+  match: ['*://*/*'],
+  grant: ['GM_registerMenuCommand', 'GM.openInTab'],
+}
